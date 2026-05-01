9360fc453617e6f016e14cd8c1a24006fdc4ebca
labtestera1 <158797306+labtesterera1@users.noreply.github.com>
2026-05-01 13:08:12 +0530

    done
    done

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
    To force users to pick up new code: bump CACHE_NAME below.
    ============================================================ */
-const CACHE_NAME = 'smartapp-shell-v0.12';
+const CACHE_NAME = 'smartapp-shell-v0.13';
 const SHELL = [
   './',
   './index.html',
